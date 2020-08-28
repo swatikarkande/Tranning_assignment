852d7c1a469d4fa3981e723b36f230dca54838ec
swatikarkande <swatiichake76@gmail.com>
2020-08-28 11:48:41 -0700

    Done javascript assignment 1

---
+++ js assignment/Javascript Assignment1/Jsexample.js
@@ -0,0 +1,13 @@
+function myFun(){
+    var fistname=document.form1.fname.value;
+    // document.writeln(fistname);
+    var lastname=document.form1.lname.value;
+    var Designation=document.form1.desigtn.value;
+    var mobilenum=document.form1.mnumber.value;
+    var address=document.form1.address.value;
+    var city=document.form1.city.value;
+    var state=document.form1.state.value;
+    var pincode=document.form1.pincode.value;
+    alert("my firstname: "+fistname+"\n lastname: "+lastname+"\n Designation: "+Designation+"\n mobile number: "+mobilenum+"\n Address: "+address
+    +"\n City: "+city+"\n State: "+state+"\n pincode"+pincode);
+}
